1a826ce7aaf92d133d93a1c4e8e0c80b74b348af
sreeja <sreeja.guduri@gmail.com>
2023-06-13 17:32:52 +0400

    make the grid different size

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const div = document.querySelector(".grid");
+let size = 16; //default size of grid
 
-for(let i=0;i<16;i++)
-{
+for (let i = 0; i < 16; i++) {
     const newdiv = document.createElement("div");
-    console.log("HERE");
-    for(let l=0;l<16;l++)
-    {
+    for (let l = 0; l < 16; l++) {
         const childdiv = document.createElement("div");
         newdiv.appendChild(childdiv);
-        childdiv.addEventListener("mouseover",function (){
+        childdiv.addEventListener("mouseover", function () {
             childdiv.style.backgroundColor = "#C9D5B5";
         });
         childdiv.classList.add("inside");
@@ -17,3 +15,52 @@ for(let i=0;i<16;i++)
     div.appendChild(newdiv);
 }
 
+let choice;
+const butt = document.querySelector("button");
+butt.addEventListener("click", function () {
+    choice = +(prompt("Enter the size of one side of the grid: "));
+    if(choice>100)
+    {
+        alert("The maximum size of the grid is 100!");
+        choice = +(prompt("Enter the size of one side of the grid: "));
+    }
+    
+    for (let i = 0; i < size; i++) {
+        const newdiv = document.querySelector(".container");
+        for (let l = 0; l < size; l++) {
+            const childdiv = document.querySelector(".inside");
+            childdiv.remove();
+        }
+        newdiv.remove();
+    }
+    
+    
+    size = 640 / choice;
+    for (let i = 0; i < choice; i++) {
+        const newdiv = document.createElement("div");
+        for (let l = 0; l < choice; l++) {
+            const childdiv = document.createElement("div");
+            newdiv.appendChild(childdiv);
+            childdiv.addEventListener("mouseover", function () {
+                childdiv.style.backgroundColor = "#C9D5B5";
+            });
+            childdiv.classList.add("inside");
+            childdiv.style.height = `${size}px`;
+            childdiv.style.width = `${size}px`;
+            childdiv.style.maxWidth = `${size}px`;
+        }
+        newdiv.classList.add("container");
+        newdiv.style.height = `${size}px`;
+        div.appendChild(newdiv);
+    }
+    size = choice;
+    // const ins = document.querySelectorAll(".inside");
+    // ins.forEach(function (i) {
+    //     i.style.height = `${choice}px`;
+    //     i.style.width = `${choice}px`;
+    // })
+    // //const con = document.querySelectorAll(".container");
+    // con.forEach(function (i) {
+    //     i.style.height = `${choice}px`;
+    // })
+});
